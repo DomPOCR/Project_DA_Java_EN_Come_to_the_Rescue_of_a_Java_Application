88154539f7480836806a1cae1540363d21d9a596
DomPOCR <dominiquep.technocenter@gmail.com>
2020-03-03 15:05:06 +0100

    Utilisation de la classe CountSymptom

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,27 +7,32 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.hemebiotech.analytics.count.CountSymptom;
 import com.hemebiotech.analytics.read.ReadSymptomDataFromFile;
 
 public class AnalyticsCounter {
-//1ere étape: On déclare une HashMap clé/valeur <> symptoms/occurences
+
+	// On déclare une HashMap clé/valeur <> symptoms/occurences
 	static Map<String, Integer> symptomsCounter = new HashMap<>();
 
 	public static void main(String args[]) throws Exception {
-//2eme étape: On lit le fichier symptoms.txt
-
+		/**
+		 * 1ere étape: On lit le fichier symptoms.txt (package "Read")
+		 *
+		 */
 		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
 
 		List<String> allSymptoms = reader.GetSymptoms();
 
-//3eme étape: On parcours le fichier en comptant les symptoms avec la map.
-		for (String symptom : allSymptoms) {
-			if (symptomsCounter.containsKey(symptom)) {
-				symptomsCounter.put(symptom, symptomsCounter.get(symptom) + 1);
-			} else {
-				symptomsCounter.put(symptom, 1);
-			}
-		}
+		/**
+		 * 2eme étape: On parcours le fichier en comptant les symptoms avec la map
+		 * (package "Count")
+		 *
+		 *
+		 */
+		CountSymptom counter = new CountSymptom();
+
+		Map<String, Integer> symptomsCounter = counter.count(allSymptoms);
 
 //4eme étape: On range dans l'ordre alphabétique les symtoms.
 		List<String> symptoms = new ArrayList<>(symptomsCounter.keySet());
--- Project02Eclipse/src/com/hemebiotech/analytics/count/CountSymptom.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/count/CountSymptom.java
@@ -1,5 +1,33 @@
 package com.hemebiotech.analytics.count;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * @author Dominique Pokrzywa
+ *
+ */
 public class CountSymptom {
+	/**
+	 * 
+	 * @param allSymptoms : Liste des symptomes
+	 * @return : le compteur par symptome
+	 */
+	public Map<String, Integer> count(List<String> allSymptoms) {
+
+		Map<String, Integer> symptomsCounter = new HashMap<String, Integer>();
+
+		for (String symptom : allSymptoms) {
+			if (symptomsCounter.containsKey(symptom)) {
+				symptomsCounter.put(symptom, symptomsCounter.get(symptom) + 1);
+			} else {
+				symptomsCounter.put(symptom, 1);
+			}
+		}
+		System.out.println("Count : " + symptomsCounter.size() + " enregistrements. ");
+		return symptomsCounter;
+	}
 
 }
