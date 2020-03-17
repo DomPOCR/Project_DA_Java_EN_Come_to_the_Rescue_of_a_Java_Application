17dce3d8c1eb1d1a07b03691f3c3113873847988
DomPOCR <dominiquep.technocenter@gmail.com>
2020-03-17 14:55:43 +0100

    Fermeture du reader : reader.close();

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -10,15 +10,17 @@ import java.util.List;
 import java.util.Map;
 
 public class AnalyticsCounter {
-//1ere étape: On déclare une HashMap clé/valeur <> symptoms/occurences
+	// 1ere étape: On déclare une HashMap clé/valeur <> symptoms/occurences
 	static Map<String, Integer> symptomsCounter = new HashMap<>();
 
 	public static void main(String args[]) throws Exception {
-//2eme étape: On lit le fichier symptoms.txt
-		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+		// 2eme étape: On lit le fichier symptoms.txt
+		BufferedReader reader = new BufferedReader(
+				new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-//3eme étape: On parcours le fichier en comptant les symptoms avec la map.
+		// 3eme étape: On parcours le fichier en comptant les symptoms avec la
+		// map.
 		while (line != null) {
 			if (symptomsCounter.containsKey(line)) {
 				symptomsCounter.put(line, symptomsCounter.get(line) + 1);
@@ -26,12 +28,14 @@ public class AnalyticsCounter {
 				symptomsCounter.put(line, 1);
 			}
 			line = reader.readLine();
+
 		}
-//4eme étape: On range dans l'ordre alphabétique les symtoms.
+		reader.close();
+		// 4eme étape: On range dans l'ordre alphabétique les symtoms.
 		List<String> symptoms = new ArrayList<>(symptomsCounter.keySet());
 		Collections.sort(symptoms);
 
-//5eme étape: On écrit le fichier result.out
+		// 5eme étape: On écrit le fichier result.out
 		FileWriter writer = new FileWriter("result.out");
 		for (String symptom : symptoms) {
 			writer.write(symptom + "=" + symptomsCounter.get(symptom) + "\n");
